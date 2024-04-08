e11e30e22034a6aa3c35237b182f853ba4e5c8a2
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-04-08 09:33:05 +0200

    klar med menyn, börjat med metoden för att boka plats

--- src/App.java
+++ src/App.java
@@ -1,23 +1,69 @@
 import java.util.Scanner;
+static Scanner tb = new Scanner(System.in);
 
 public class App {
     public static void main(String[] args) throws Exception {
-        while (true) {
+        
+        boolean loop = true;
+        int[][] bokning = new int[5][4];
+        int[] priser = new int[20];
+        String[][] visa_platser = new String[5][4];
+        while (loop) {
             System.out.println("Hej! Välj en tjänst från listan nedan!");
             visa_meny();
         }
         
     }
+
     public static void visa_meny(){
-        Scanner tb = new Scanner(System.in);
+        
         int val = tb.nextInt();
+        try {
+            val = tb.nextInt();
+        } 
+        catch (Exception e) {
+            System.out.println("Välj en tjänst med dess siffra.");
+        }
+        System.out.println("1. Boka en plats på bussen.");
+        System.out.println("2. Hitta/ändra bokning.");
+        System.out.println("3. Visa passagerare");
+        System.out.println("4. Beräkna vinst av biljetter.");
+        System.out.println("5. Avsluta.");
+        while (true) {
+            if (val>5) {
+                System.out.println("Välj en tjänst med dess siffra.");
+            }
+        }
         switch (val) {
             case 1:
-                
+                boka_plats();
                 break;
-        
-            default:
+            case 2:
+                hitta_bokning();
+                break;
+            case 3:
+                visa_passagerare();
+                break;
+            case 4:
+                beräkna_vinst();
+                break;
+            case 5:
+                avsluta();
                 break;
         }
+    }
+    
+    public static int boka_plats(){
+        System.out.println("Ange ditt födelsedatum ÅÅÅÅMMDD:");
+        int person_nr = tb.nextInt();
+        while (person_nr<10000000 || person_nr>=100000000) {
+            System.out.println("Ange ditt födelsedatum ÅÅÅÅMMDD:");
+            person_nr = tb.nextInt();
+        }
+        
+    }
+
+    public static boolean avsluta(){
+        return false;
     }
 }
