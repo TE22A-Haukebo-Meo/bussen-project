d48383177e4462b14b43f7934af6bddeda5ec0e9
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-04-08 08:46:59 +0200

    Börjat med koden

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        while (true) {
+            System.out.println("Hej! Välj en tjänst från listan nedan!");
+            visa_meny();
+        }
+        
+    }
+    public static void visa_meny(){
+        Scanner tb = new Scanner(System.in);
+        int val = tb.nextInt();
+        switch (val) {
+            case 1:
+                
+                break;
+        
+            default:
+                break;
+        }
     }
 }
